8362784fa14f3ebdf08db326dbec58af2f63a5e7
DPS <smith.dp@gmail.com>
2019-06-08 18:35:40 +0100

    X and O Alternate

--- src/index.js
+++ src/index.js
@@ -16,13 +16,17 @@ function Square(props){
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIstNext: true,
         }
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
